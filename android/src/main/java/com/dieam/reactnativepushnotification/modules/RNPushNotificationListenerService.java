00199e38f5b7aff59bc3be77d2f995b8f1558469
Olcay Ay <olcayay@gmail.com>
2017-04-19 13:53:54 +0300

    ignore smooch push messages

--- android/src/main/java/com/dieam/reactnativepushnotification/modules/RNPushNotificationListenerService.java
+++ android/src/main/java/com/dieam/reactnativepushnotification/modules/RNPushNotificationListenerService.java
@@ -26,7 +26,7 @@ public class RNPushNotificationListenerService extends GcmListenerService {
 
     @Override
     public void onMessageReceived(String from, final Bundle bundle) {
-        Log.v(LOG_TAG, "onMessageReceived was modified in order to receive smooch push notifictions");
+        Log.v(LOG_TAG, "onMessageReceived was modified in order to receive smooch push notifications");
         JSONObject data = getPushData(bundle.getString("data"));
         if (data != null) {
             if (!bundle.containsKey("message")) {
@@ -41,14 +41,16 @@ public class RNPushNotificationListenerService extends GcmListenerService {
             if (!bundle.containsKey("color")) {
                 bundle.putString("color", data.optString("color", null));
             }
-            if (bundle.containsKey("smoochNotification")) {
-                Log.v(LOG_TAG, "onMessageReceived: " + bundle);
-                Log.d(LOG_TAG, "onMessageReceived: ignore smooch messages");
-                return;
-            }
             final int badge = data.optInt("badge", -1);
             if (badge >= 0) {
                 ApplicationBadgeHelper.INSTANCE.setApplicationIconBadgeNumber(this, badge);
+            }
+        }
+        else{
+            if ( bundle.containsKey("smoochNotification") ){
+                Log.v(LOG_TAG, "onMessageReceived: " + bundle);
+                Log.d(LOG_TAG, "onMessageReceived: ignore smooch messages");
+                return;
             }
         }
 
